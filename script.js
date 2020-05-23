e71c3d81363b99f83b5bc672d5a0546723c0c9c2
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-05-23 16:53:22 -0700

    trying to pull from api data

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-$(document).ready(function () {
-    alert("Hello Skye!");
-});
-
-let previousCities = [];
-
+$(document).ready(function () {});
 
 //localStorage the previous cities
+let previousCities = [];
+// localStorage.setItem("previousCities");
+// document.getElementById("result").innerHTML = localStorage.getItem(
+//   "previousCities"
+// );
+
 //run the API call to weathermap API
-//Api is going to give us a object you need toJSON.parse
-//Need to use the GET method through AJAX call
-
-
 function getWeather(location) {
-
-    const QUERY = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=484d77b1cc177cb84f44663aaf1730c7";
-
-    $.ajax({
-            url: QUERY,
-            method: "GET"
-        })
-        .then(function (result) {
-            console.log(result);
-            $("#cityName").append("<h2> City Name: " + result.name + "</h2>");
-            $
-            $("#mainWeather").append("<h5> Sky: " + result.weather[0].main + "</h5>");
-
-        })
-
-
-
+  const QUERY =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&appid=484d77b1cc177cb84f44663aaf1730c7";
+  //Api is going to give us a object you need toJSON.parse
+  //Need to use the GET method through AJAX call
+  $.ajax({
+    url: QUERY,
+    method: "GET",
+  }).then(function (result) {
+    console.log(result);
+    $("#cityName").text("City Name: " + result.name + "");
+    $("#mainWeather").text("Sky: " + result.weather[0].description + "");
+    //not working
+    $("#temp").text("Temp: " + result.main.temp + "");
+    $("#humidity").text("Humidity: " + result.main.humidity + "");
+    $("#wind").text("Wind: " + result.wind.speed + "");
+  });
 }
-$("#searchbtn").click(function (e) {
+$("#searchBtn").click(function (e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  let location = $("#searchInput").val().trim();
+  console.log(location);
 
-    let location = $("#searchinput").val().trim();
-    console.log(location);
-
-    getWeather(location)
-})
+  getWeather(location);
+});
 
 /*
-
-Function~name~(previousCities, result);
+Corrado notes
+function (previousCities, result);
 localStorage.setItem(cities,
         JSON.stringify(previousCities);
 
@@ -56,4 +53,8 @@ localStorage.setItem(cities,
             } else {
                 Console.log(“way too cold, I need to go to Mesa”);
             }
+*/
+/*
+Create buttons to the left that results save to
+5 day weather forecast 
 */
